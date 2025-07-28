3cf2db7814e655e80bb3bbcb24519a9610390bc1
Amoolya P N <amoolyapn2003@gmail.com>
2025-07-28 20:49:00 +0530

    Create Day_23.py

---
+++ Day_23.py
@@ -0,0 +1,12 @@
+#Dictionary
+student={
+    "name":"Ammu",
+    "age":23,
+    "city":"Blr"
+}
+
+student["age"]=21
+student["Grade"]=8.7
+for key,value in student.items():
+    print(key,":",value)
+    
